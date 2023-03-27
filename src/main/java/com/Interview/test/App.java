04c68fc60d74d641b66b0666b5d45e1a1bdc73b7
adila Aftab <adilaAftab20@gmail.com>
2023-03-28 02:29:26 +0530

    this is 2nd commit

--- src/main/java/com/Interview/test/App.java
+++ src/main/java/com/Interview/test/App.java
@@ -13,6 +13,10 @@ public class App
 		
 	}
 	
+	public void addmethod2() {
+		System.out.println("this is used for second  git commit");
+		
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
