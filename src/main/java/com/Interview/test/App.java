a3cb3eb57bf66e9d25b7f4ec4e1e8994b6a09433
adila Aftab <adilaAftab20@gmail.com>
2023-03-28 02:23:46 +0530

    this is 1st commit

---
+++ src/main/java/com/Interview/test/App.java
@@ -0,0 +1,20 @@
+package com.Interview.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+
+
+{
+	public void addmethod() {
+		System.out.println("this is used for git commit");
+		
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
